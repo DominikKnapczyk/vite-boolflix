0cb0884316d53f1aa3ae63cf3f90761a1a8af1b6
DominikKnapczyk <dominik.kna.01@gmail.com>
2023-03-07 08:27:34 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>TEST</h1>
+</template>
+
+<style lang="scss" scoped>
+body {
+  h1 {
+    color: red;
+  }
+}
+</style>
